8485af2e8ad4d9f339da1eb3df3bd09ed4500153
Allief Nuriman <57051116+alliefn@users.noreply.github.com>
2022-03-19 09:28:49 +0700

    Tidying UI and init cube

--- src/cube/script.js
+++ src/cube/script.js
@@ -1,51 +1,78 @@
-'use strict';
+main()
 
-// generate cube with webgl
-function generateCube(gl, program, vertices, colors, normals, textureCoords) {
-    var cube = {};
-    
-    cube.vertexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, cube.vertexBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-    
-    cube.colorBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, cube.colorBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
-    
-    cube.normalBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, cube.normalBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normals), gl.STATIC_DRAW);
-    
-    cube.textureCoordBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, cube.textureCoordBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoords), gl.STATIC_DRAW);
-    
-    cube.indexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cube.indexBuffer);
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
-    
-    cube.numIndices = indices.length;
-    
-    gl.bindBuffer(gl.ARRAY_BUFFER, null);
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
-    
-    return cube;
+function shaderCreator(gl, type, source) {
+    var shader = gl.createShader(type)
+    gl.shaderSource(shader, source)
+    gl.compileShader(shader)
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        throw new Error(gl.getShaderInfoLog(shader))
+    }
+    return shader
 }
 
-// take canvas and webgl context and return shader program
-function initShaders(gl, vertexShaderSource, fragmentShaderSource) {
-    var vertexShader = gl.createShader(gl.VERTEX_SHADER);
-    gl.shaderSource(vertexShader, vertexShaderSource);
-    gl.compileShader(vertexShader);
-    
-    var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-    gl.shaderSource(fragmentShader, fragmentShaderSource);
-    gl.compileShader(fragmentShader);
-    
-    var program = gl.createProgram();
-    gl.attachShader(program, vertexShader);
-    gl.attachShader(program, fragmentShader);
-    gl.linkProgram(program);
-    
-    return program;
+function createProgram(gl, vertexShader, fragmentShader) {
+    var program = gl.createProgram()
+    gl.attachShader(program, vertexShader)
+    gl.attachShader(program, fragmentShader)
+    gl.linkProgram(program)
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+        throw new Error(gl.getProgramInfoLog(program))
+    }
+    return program
+}
+
+function main() {
+    const canvas = document.getElementById('canvas');
+    const gl = canvas.getContext('webgl');
+
+    if (!gl) {
+        alert('Unable to initialize WebGL. Your browser or machine may not support it.');
+        return;
+    }
+
+    // Create vertex shader
+    var vertCode = `
+        attribute vec3 coordinates;
+        uniform mat4 transformationMatrix;
+        varying float colorFactor;
+        void main() {
+            // Z dikali negatif karena vektor Z di WebGL berlawanan
+            vec4 transformedPosition = vec4(coordinates.xy, coordinates.z * -1.0, 1.0) * transformationMatrix;
+            gl_Position = transformedPosition;
+            colorFactor = min(max((1.0 - transformedPosition.z) / 2.0, 0.0), 1.0);
+        }
+	`;
+
+    var vertShader = shaderCreator(gl, gl.VERTEX_SHADER, vertCode);
+
+    // Create fragment shader
+    var fragCode = `
+        precision mediump float;
+        uniform vec3 userColor;
+        varying float colorFactor;
+        void main(void) {
+            gl_FragColor = vec4(userColor * colorFactor, 1.0);
+        }
+    `;
+
+    var fragShader = shaderCreator(gl, gl.FRAGMENT_SHADER, fragCode);
+
+    // Create program
+    var shaderProgram = createProgram(gl, vertShader, fragShader);
+
+    // Check the shader program
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+        alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
+        return;
+    }
+
+    gl.useProgram(shaderProgram);
+
+    // Create vertex buffer
+    var vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+
+    // Create index buffer
+    var indexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
 }
