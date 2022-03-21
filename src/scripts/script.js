3b7d38a5fde741f7b338cad0d436b900875e456e
Lock1 <30568743+Lock1@users.noreply.github.com>
2022-03-22 00:52:15 +0700

    Refaktor initial transformation matrix
    Refaktor initial transformation matrix ke fungsi baru

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -2,8 +2,31 @@ import { translationMatrix, scaleMatrix, matrixMult, rotationMatrix } from './ma
 import { webglCreateShaderProgram } from './utils.js'
 import { getHollowCube, getModelFromObjFile } from './model.js'
 
+function getInitialTransformMatrix() {
+    var transformMatrix = translationMatrix(0, 0); // Just empty matrix
+    var translation = [0, 0];
+    var scale       = [0.7, 0.7, 0.7];
+    var rotation    = [0, 0, 0];
+
+    transformMatrix = matrixMult(scaleMatrix(scale[0], scale[1], scale[2]), transformMatrix);
+    transformMatrix = matrixMult(translationMatrix(translation[0], translation[1]), transformMatrix);
+    transformMatrix = matrixMult(rotationMatrix(rotation[0], rotation[1], rotation[2]), transformMatrix);
+    return transformMatrix;
+}
+
 function main() {
-    // -- Ritual WebGL --
+    // -- Get model --
+    // TODO : Update
+    var model     = getHollowCube();
+
+    // -- Idle animation parameter --
+    // Asumsi requestAnimationFrame hingga 60 calls per sec
+    var rot_increment = [Math.PI / 180 * (1 / 60) * 5, Math.PI / 180 * (1 / 60) * 2, 0];
+
+    // -- Initial transformation matrix --
+    var transformMatrix = getInitialTransformMatrix();
+
+    // -- Ritual WebGL Create Program --
     const canvas = document.getElementById('canvas');
     const gl     = canvas.getContext('webgl');
 
@@ -15,30 +38,24 @@ function main() {
     var shaderProgram = webglCreateShaderProgram(gl, 'vertex-shader-3d-cube', 'fragment-shader-3d-cube');
     gl.useProgram(shaderProgram);
 
-    // -- Get model --
-    // TODO : Update
-    var model     = getHollowCube();
-
-
     // -- Ritual Binding Buffer --
-    var indexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-
     var vertexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
 
-    var coord = gl.getAttribLocation(shaderProgram, "coordinates"); // FIXME : ??? Kenapa attr kalo serasa uniform?
-    gl.vertexAttribPointer(coord, 3, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(coord);
+    var indexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
 
-    var transformMatrix = translationMatrix(0, 0);
+    var coordLoc = gl.getAttribLocation(shaderProgram, "coordinates");
+    gl.vertexAttribPointer(coordLoc, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(coordLoc);
+
     var transformMatrixLoc = gl.getUniformLocation(shaderProgram, "transformationMatrix");
     gl.uniformMatrix4fv(transformMatrixLoc, false, new Float32Array(transformMatrix));
 
     var colorLoc = gl.getUniformLocation(shaderProgram, "userColor");
     gl.uniform3f(colorLoc, 1, 0.5, 0);
 
-    // Pass data
+    // Bind vertices and indices
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(model.vertices), gl.STATIC_DRAW);
 
@@ -48,33 +65,21 @@ function main() {
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.enable(gl.DEPTH_TEST);
 
-
-    // -- Initial transformation matrix calculation --
-    var xPos = 0;
-    var yPos = 0;
-    var scaleX = 1;
-    var scaleY = 1;
-    var scaleZ = 1;
-
-    var rtx = Math.PI / 180 * (1 / 60) * 5;
-    var rty = Math.PI / 180 * (1 / 60) * 5;
-    var rtz = 0
-    transformMatrix = matrixMult(scaleMatrix(scaleX, scaleY, scaleZ), transformMatrix);
-    transformMatrix = matrixMult(translationMatrix(xPos, yPos), transformMatrix);
-    transformMatrix = matrixMult(rotationMatrix(rtx, rty, rtz), transformMatrix);
-
-    var timer = 0;
     window.requestAnimationFrame(render);
 
-    function render(time_now) {
-        var delta = time_now - timer; // FIXME : Mungkin ga dibutuhin, lets see
-        transformMatrix = matrixMult(rotationMatrix(rtx, rty, rtz), transformMatrix);
 
+
+    function render() {
+        // Idle animation
+        transformMatrix = matrixMult(rotationMatrix(rot_increment[0], rot_increment[1], rot_increment[2]), transformMatrix);
+
+        // Update buffer
         gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(model.vertices), gl.STATIC_DRAW);
-
-        gl.clear(gl.COLOR_BUFFER_BIT);
         gl.uniformMatrix4fv(transformMatrixLoc, false, new Float32Array(transformMatrix));
+
+        // Draw
+        gl.clear(gl.COLOR_BUFFER_BIT);
         gl.drawElements(gl.TRIANGLES, model.numPoints, gl.UNSIGNED_SHORT, 0);
         window.requestAnimationFrame(render);
     }
